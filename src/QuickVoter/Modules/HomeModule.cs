e173bde58e39aa658d92c91c4376a9d2788be4e9
Anders Ljusberg <andlju@gmail.com>
2013-11-21 21:46:58 +0100

    First version

---
+++ src/QuickVoter/Modules/HomeModule.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Remoting.Messaging;
+using Nancy;
+
+namespace QuickVoter.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => View["index"];
+
+            Get["/question/{questionId}"] = pars => View["question"];
+
+        }
+    }
+}
---
+++ src/QuickVoter/QuickVoterBootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using Nancy.Conventions;
+using Nancy.Hosting.Aspnet;
+
+namespace QuickVoter
+{
+    public class QuickVoterBootstrapper : DefaultNancyAspNetBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/Scripts"));
+            base.ConfigureConventions(nancyConventions);
+        }
+    }
+}
