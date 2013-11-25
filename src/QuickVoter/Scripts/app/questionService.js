25f0f7f2929ae517174749c0b932a3a7fa61e16f
Anders Ljusberg <andlju@gmail.com>
2013-11-26 00:00:38 +0100

    A simple js service

---
+++ src/QuickVoter/Scripts/app/questionService.js
@@ -0,0 +1,41 @@
+﻿var QuickVoter = QuickVoter | {};
+
+QuickVoter.QuestionService = function($) {
+
+    var getQuestions = function() {
+        return $.ajax('/questions');
+    };
+
+    var getQuestion = function(questionId) {
+        return $.ajax('/questions/' + questionId);
+    };
+
+    var addQuestion = function(text) {
+        return $.ajax('/questions', {
+            type: "POST",
+            data: { text: text }
+        });
+    };
+
+    var addAnswer = function(questionId, text) {
+        return $.ajax('/questions/' + questionId + '/answers', {            
+            type: "POST",
+            data: { text: text }
+        });
+    };
+
+    var addVote = function(questionId, answerId) {
+        return $.ajax('/questions/' + questionId + '/answers/' + answerId, {
+            type: "POST",
+        });
+    };
+    
+    return {        
+        getQuestions: getQuestions,
+        getQuestion: getQuestion,
+        addQuestion: addQuestion,
+        addAnswer: addAnswer,
+        addVote: addVote
+    };
+}(jQuery);
+
